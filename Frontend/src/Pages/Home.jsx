951b378563d54c7552b598d75f2308c26aad034f
shohan12727 <aishohan001@gmail.com>
2026-03-17 01:43:12 +0600

    first commit

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-5xl'>King shohan</h1>
+            <h2 className='text-7xl'>Medi Buddy</h2>
+        </div>
+    );
+};
+
+export default Home;
